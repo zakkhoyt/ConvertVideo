6ebbd9570689b424a665411b21d215895429fcb5
Zakk Hoyt <zakkhoyt@Zakks-MacBook-Pro.local>
2012-11-11 22:09:46 -0800

    added -n command to see command with no action

--- convert_video.py
+++ convert_video.py
@@ -26,14 +26,29 @@ import sys
 import os
 
 if __name__ == '__main__':
-	if len(sys.argv) != 4:
+	sys.stdout.write('there are args' + str(sys.argv))
+
+	if len(sys.argv) == 2:
+		if vid_input.find('copy') > -1:
+			os.system('pbcopy find ../bin/compilations/ | grep ".flv\|.avi\|.mpg\|.wmv\|.mpeg\|.m4v" | xargs -I input_file python convert_video.py input_file out Default')
+			sys.stdout.write('exiting after copy')
+			sys.exit(0)
+
+	if len(sys.argv) <= 3:
 		sys.stderr.write('Usage: ' + sys.argv[0] + ' <input_video> <output_dir> <Defult|iPhone>\n')
 		sys.exit(2)
-
+	
+	
 	vid_input = sys.argv[1]
 	vid_output_dir = sys.argv[2]
 	vid_size = sys.argv[3]
+	if len(sys.argv) > 4:
+		other_arg = sys.argv[4]
+	else:
+		other_arg = '';
 	convert_command = ''
+
+	sys.stdout.write('other_arg.n' + str(other_arg.find('-n')))
 
 	# Get the base path to the input file so we can recreate the same dir structure for output
 	base_path = vid_input[vid_input.find('/'):len(vid_input)]	
@@ -41,11 +56,14 @@ if __name__ == '__main__':
 
 	# Get the base file name of the input vid and append new extension
 	input_file = os.path.splitext(vid_input)[0]
+	input_file = input_file.replace('//', '/')
 	tokens = input_file.split('/')
 	input_file = tokens[len(tokens)-1]
 
 	# Create path to output dir
 	new_dir = vid_output_dir + base_path
+
+	# Check if we want to put example on the clipboard
 
 	# Create new file name
 	output_file = vid_output_dir + '/' + base_path + '/' + input_file + '.mp4'
@@ -57,12 +75,15 @@ if __name__ == '__main__':
 
 	os.system('mkdir -p ' + new_dir)
 	if vid_size == 'Default':
-		convert_command = 'ffmpeg -i ' + '"' + vid_input + '"' + ' -y -f mp4 -strict -2 -sameq ' + '"' + output_file + '"'
+		if other_arg.find('-n') < 0:
+			convert_command = 'ffmpeg -i ' + '"' + vid_input + '"' + ' -y -f mp4 -strict -2 -sameq ' + '"' + output_file + '"'
 	elif vid_size == 'iPhone':
-		convert_command = 'ffmpeg -i ' + '"' + vid_input + '"' + ' -y -f mp4 -s cif -b 128 -strict -2 -sameq ' + '"' + output_file + '"'
+		if other_arg.find('-n') < 0:
+			convert_command = 'ffmpeg -i ' + '"' + vid_input + '"' + ' -y -f mp4 -s cif -b 128 -strict -2 -sameq ' + '"' + output_file + '"'
 
 	sys.stdout.write('--------------------------------------------\n')
 	sys.stdout.write('Platform: ' + sys.platform + '\n')
+	sys.stdout.write('new_dir: ' + new_dir + '\n')
 	sys.stdout.write('Input file: ' + vid_input + '\n')
 	sys.stdout.write('Output file: ' + output_file + '\n')
 	sys.stdout.write('Converstion command: ' + convert_command + '\n')
